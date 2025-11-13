d88bf683271dea290dd1d65f61a7d6a2dcf0ad20
Anubhav Rawal <anubhavrawal00@gmail.com>
2025-11-13 01:04:28 +0000

    Add basic formatting and nix tools

---
+++ flake.nix
@@ -0,0 +1,154 @@
+# aws.greengrass.SecureTunneling - AWS Greengrass component for secure tunneling
+# to IoT devices using AWS IoT Device Management Secure Tunneling service.
+#
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+{
+  description = "AWS Greengrass Secure Tunneling Component";
+  inputs.flakelight.url = "github:nix-community/flakelight";
+  outputs = { flakelight, ... }@inputs: flakelight ./.
+    ({ lib, config, ... }:
+      let
+        inherit (builtins) fromJSON;
+        inherit (lib) mapAttrs readFile mapAttrsToList toUpper;
+
+        filteredSrc = lib.fileset.toSource {
+          root = ./.;
+          fileset = lib.fileset.unions [
+            ./CMakeLists.txt
+            ./src
+            ./include
+            ./fc_deps.json
+            ./.clang-tidy
+          ];
+        };
+
+        llvmStdenv = pkgs: pkgs.overrideCC pkgs.llvmPackages.stdenv
+          (pkgs.llvmPackages.stdenv.cc.override
+            { inherit (pkgs.llvmPackages) bintools; });
+
+        deps = pkgs: mapAttrs (_: v: pkgs.fetchgit (v // { fetchSubmodules = true; }))
+          (fromJSON (readFile ./fc_deps.json));
+
+        fetchContentFlags = pkgs: mapAttrsToList
+          (n: v: "-DFETCHCONTENT_SOURCE_DIR_${toUpper n}=${v}")
+          (deps pkgs);
+
+      in
+      {
+        systems = lib.systems.flakeExposed;
+        inherit inputs;
+
+        devShell = pkgs: {
+          packages = with pkgs; [ clang-tools clangd-tidy git openssl.dev ];
+          env.NIX_HARDENING_ENABLE = "";
+          shellHook = ''
+            export MAKEFLAGS=-j
+          '';
+        };
+
+        devShells.clang = { lib, pkgs, ... }: {
+          imports = [ (config.devShell pkgs) ];
+          stdenv = lib.mkForce (llvmStdenv pkgs);
+        };
+
+        formatters = { llvmPackages, cmake-format, nodePackages, ... }:
+          let
+            fmt-c = "${llvmPackages.clang-unwrapped}/bin/clang-format -i";
+            fmt-cmake = "${cmake-format}/bin/cmake-format -i";
+            fmt-yaml =
+              "${nodePackages.prettier}/bin/prettier --write --parser yaml";
+          in
+          {
+            "*.c" = fmt-c;
+            "*.h" = fmt-c;
+            "CMakeLists.txt" = fmt-cmake;
+            ".clang*" = fmt-yaml;
+          };
+
+        pname = "secure-tunnel";
+        package = { pkgs, openssl, lib, stdenv, pkg-config, cmake, ninja, argp-standalone, defaultMeta }:
+          stdenv.mkDerivation {
+            name = "secure-tunnel";
+            src = filteredSrc;
+            nativeBuildInputs = [ pkg-config cmake ninja ];
+            buildInputs = [ openssl ] ++ lib.optional (!stdenv.hostPlatform.isGnu) argp-standalone;
+            cmakeBuildType = "MinSizeRel";
+            cmakeFlags = (fetchContentFlags pkgs) ++ [ "-DENABLE_WERROR=1" ];
+            dontStrip = true;
+            meta = defaultMeta;
+          };
+
+        checks =
+          let
+            clangBuildDir = { pkgs, pkg-config, clang-tools, openssl, cmake, ... }:
+              (llvmStdenv pkgs).mkDerivation {
+                name = "clang-cmake-build-dir";
+                nativeBuildInputs = [ pkg-config clang-tools ];
+                buildInputs = [ openssl ];
+                buildPhase = ''
+                  export PKG_CONFIG_PATH="${openssl.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
+                  ${cmake}/bin/cmake -B $out -S ${filteredSrc} \
+                    -D CMAKE_BUILD_TYPE=Debug ${toString (fetchContentFlags pkgs)} \
+                    -D CMAKE_C_FLAGS="-I${openssl.dev}/include"
+                  rm $out/CMakeFiles/CMakeConfigureLog.yaml
+                '';
+                dontUnpack = true;
+                dontPatch = true;
+                dontConfigure = true;
+                dontInstall = true;
+                dontFixup = true;
+                allowSubstitutes = false;
+              };
+          in
+          {
+            build-clang = pkgs: pkgs.secure-tunnel.override
+              { stdenv = llvmStdenv pkgs; };
+
+            build-musl-pi = pkgs: pkgs.pkgsCross.muslpi.secure-tunnel;
+
+            clang-tidy = pkgs: ''
+              set -eo pipefail
+              PATH=${lib.makeBinPath
+                (with pkgs; [clangd-tidy clang-tools fd])}:$PATH
+              clangd-tidy -j$(nproc) -p ${clangBuildDir pkgs} --color=always \
+                $(fd . ${filteredSrc}/src -e c -e h) |\
+                sed 's|\.\.${filteredSrc}/||'
+            '';
+
+            cmake-lint = pkgs: ''
+              ${pkgs.cmake-format}/bin/cmake-lint \
+                $(${pkgs.fd}/bin/fd '.*\.cmake|CMakeLists.txt') \
+                --suppress-decorations
+            '';
+
+            spelling = pkgs: ''
+              ${pkgs.nodePackages.cspell}/bin/cspell "**" --quiet
+              ${pkgs.coreutils}/bin/sort -cuf misc/dictionary.txt
+            '';
+          };
+
+        withOverlays = final: prev: {
+          clangd-tidy = final.callPackage
+            ({ python3Packages }:
+              python3Packages.buildPythonPackage rec {
+                pname = "clangd_tidy";
+                version = "1.1.0.post1";
+                format = "pyproject";
+                src = final.fetchPypi {
+                  inherit pname version;
+                  hash = "sha256-wqwrdD+8kd2N0Ra82qHkA0T2LjlDdj4LbUuMkTfpBww=";
+                };
+                buildInputs = with python3Packages; [ setuptools-scm ];
+                propagatedBuildInputs = with python3Packages; [
+                  attrs
+                  cattrs
+                  typing-extensions
+                ];
+              })
+            { };
+        };
+      });
+}
